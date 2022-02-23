e9dd3876cd2a540cf5625ab26514707464d18b5a
AJC-25 <s0566356@htw-berlin.de>
2022-02-23 16:32:06 +0100

    created index.html, style.css and script.js
    additionally added music links and headphones.svg

---
+++ script.js
@@ -0,0 +1,68 @@
+const songs= [
+    "bensound-acousticbreeze.mp3",
+    "bensound-betterdays.mp3",
+    "bensound-creativeminds.mp3",
+    "bensound-littleidea.mp3",
+    "bensound-memories.mp3"
+];
+const player = document.getElementById("player");
+
+function createSongList(){
+    const list = document.createElement("ol");
+    for(let i = 0; i < songs.length; i++){
+        const item = document.createElement("li");
+        item.appendChild(document.createTextNode(songs[i]));
+        list.appendChild(item);
+    }
+    return list; 
+}
+
+const songList = document.getElementById("songlist");
+songList.appendChild(createSongList());
+
+const links = document.querySelectorAll("li");
+for(const link of links){
+    link.addEventListener("click", setSong)
+}
+
+function setSong(e){
+    document.querySelector('#headphones').classList.remove("pulse");
+
+    const source = document.getElementById("source");
+    source.src = "bensound.com/" + e.target.innerText;
+
+    document.querySelector("#currentSong").innerText = `Now playing: ${e.target.innerText}`;
+
+    
+    player.load();
+    player.play();
+
+    document.querySelector('#headphones').classList.add("pulse");
+}
+
+function playAudio(){ 
+    if(player.readyState){  //if audio as finished loading:
+        player.play();
+    }
+}
+
+function pauseAudio(){
+    player.pause();
+}
+
+const slider = document.getElementById("volumeSlider");
+slider.oninput = function(e){
+    const volume = e.target.value;
+    player.volume = volume; 
+};
+
+function updateProgress(){
+    if(player.currentTime > 0){
+        const progressBar = document.getElementById("progress");
+
+        // console.log(player.currentTime);
+        // console.log(player.duration);
+    
+        progressBar.value = (player.currentTime / player.duration)*100
+    }
+}
